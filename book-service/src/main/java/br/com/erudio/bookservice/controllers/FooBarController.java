3b82dfbcdf1e4e1dd7d4bc97e392902d5991f84a
KaiqueSM <kaiquesmendonca@hotmail.com>
2023-06-15 16:28:56 -0300

    Mais exemplos de funcionalidades do resilience

--- book-service/src/main/java/br/com/erudio/bookservice/controllers/FooBarController.java
+++ book-service/src/main/java/br/com/erudio/bookservice/controllers/FooBarController.java
@@ -1,5 +1,8 @@
 package br.com.erudio.bookservice.controllers;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +18,18 @@ public class FooBarController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping
-    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+//    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
+//    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String fooBar(){
-        logger.info("Request of foo-bar is received");
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8080/foo-bar",String.class);
-        return response.getBody();
+
+//        logger.info("Request of foo-bar is received");
+//        var response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar",String.class);
+//        return response.getBody();
+
+        return "Foo-Bar";
     }
 
     public String fallbackMethod(Exception e){

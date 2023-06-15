d7ebd38c87764952231c223870b6f23991180a63
KaiqueSM <kaiquesmendonca@hotmail.com>
2023-06-15 15:51:15 -0300

    Log para api gateway e resilience4j para book service

--- book-service/src/main/java/br/com/erudio/bookservice/controllers/BookController.java
+++ book-service/src/main/java/br/com/erudio/bookservice/controllers/BookController.java
@@ -12,9 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/book-service")
---
+++ book-service/src/main/java/br/com/erudio/bookservice/controllers/FooBarController.java
@@ -0,0 +1,25 @@
+package br.com.erudio.bookservice.controllers;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/")
+public class FooBarController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping
+    @Retry(name = "foo-bar")
+    public String fooBar(){
+        logger.info("Request of foo-bar is received");
+        var response = new RestTemplate()
+                .getForEntity("http://localhost:8080/foo-bar",String.class);
+        return response.getBody();
+    }
+}
